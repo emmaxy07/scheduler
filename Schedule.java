54c654c1e0dc48e869b37115a469a1f683eab7f6
Emmanuel Omole <emmanueliyare7@gmail.com>
2025-10-30 20:20:18 -0400

    new fixes

--- Schedule.java
+++ Schedule.java
@@ -40,6 +40,7 @@ public class Schedule {
         ArrayList<String> newSchedules = new ArrayList<>();
         ArrayList<String> schedules = new ArrayList<>();
         int ID = 1;
+        String userInput = "";
 
 
         if(file.exists()){
@@ -57,118 +58,148 @@ public class Schedule {
         }
 
         newSchedules.addAll(schedules);
-        
-        do{
-            System.out.print("What is your full name?: ");
-            fullName = scanner.nextLine();
 
-                while(true){
-                    System.out.print("What is your email address?: ");
-                    emailInput = scanner.nextLine();
-                    if(emailInput != null && pattern.matcher(emailInput).matches()){
-                        email = emailInput;
-                        break;
-                    } else {
-                        System.out.println("Your email: " + emailInput + " , is not correct. Try again");
-                    }
-                }
-
-
-            System.out.print("What is your professional background?: ");
-            bgInput = scanner.nextLine();
-
-            System.out.print("Tell us how we can help you today: ");
-            needsInput = scanner.nextLine();
-
-            // add validation of date input
-            while(true){
-                System.out.print("What date do you want to pick?: ");
-                futureDate = scanner.nextLine();
-                parsedFutureDate = LocalDate.parse(futureDate);
-                if(todayDate.isBefore(parsedFutureDate)){
-                    break;
-                } else {
-                    System.out.println("You chose a date that has been passed. Try a future date.");
-                }
-            }
-
-            // add validation of time input
-            while (true) {   
-                System.out.print("What time do you want to pick between 8am and 5pm?: ");
-                timeInput = scanner.nextLine();
-                time = LocalTime.parse(timeInput);
-                String openingHrs = "08:00:00";
-                String closingHrs = "17:00:00";
-                LocalTime startTime = LocalTime.parse(openingHrs);
-                LocalTime endTime = LocalTime.parse(closingHrs);
+        while(true){
+            System.out.print("What do you want to do?: ");
+            userInput = scanner.nextLine();
+            if(userInput.equals("schedule-cli list")){
+                    System.out.printf("%-5s | %-20s | %-25s | %-15s | %-10s | %-15s | %-10s%n",
+                "ID", "Full Name", "Email", "Background", "Need", "Date", "Time");
+                System.out.print("--------------------------------------------------------------------------------------------------------------------" + "\n");
     
-                if(time.isAfter(startTime) && time.isBefore(endTime)){
-                    break;
-                } else {
-                    System.out.println("You selected a time outside our opening hours. Try again");
+    
+                for(int i = 0; i < newSchedules.size(); i+=7){
+                    if(i + 6 < newSchedules.size()){
+                        System.out.printf("%-5s | %-20s | %-25s | %-15s | %-10s | %-15s | %-10s%n",
+                        newSchedules.get(i),     
+                        newSchedules.get(i + 1), 
+                        newSchedules.get(i + 2), 
+                        newSchedules.get(i + 3), 
+                        newSchedules.get(i + 4), 
+                        newSchedules.get(i + 5), 
+                        newSchedules.get(i + 6)  
+                );
+                    }
                 }
-            }
-
-
-            formattedDate = parsedFutureDate.toString();
-            formattedTime = time.toString();
-
-            for(int i = 0; i < newSchedules.size(); i++){
-                if(i % 7 == 0){
-                    ID++;
-                }
-            }
+            System.out.print("--------------------------------------------------------------------------------------------------------------------" + "\n");
+    
+            } else if(userInput.equals("schedule-cli add")){
+                do{
+                    System.out.print("What is your full name?: ");
+                    fullName = scanner.nextLine();
         
-            newSchedule.addFirst(Integer.toString(ID));
-            newSchedule.add(fullName);
-            newSchedule.add(email);
-            newSchedule.add(bgInput);
-            newSchedule.add(needsInput);
-            newSchedule.add(formattedDate);
-            newSchedule.add(formattedTime);
-
-            for(String s: newSchedule){
-            System.out.println(s);
-        }
-
-             newSchedules.addAll(newSchedule);
-
-
-            fullName = "";
-            email = "";
-            bgInput = "";
-            needsInput = "";
-            formattedDate = "";
-            formattedTime = "";
-
-
-            String headers = "ID,Full Name,Email,Background,Need,ScheduledDate,ScheduledTime";
-
-                if(file.length() == 0){
-                    try(FileWriter writer = new FileWriter(file)) {
-                    writer.append(headers + "\n");
-                    writer.append(String.join(",", newSchedule) + "\n");
-                      } catch (Exception e) {
-                        System.out.println("Something went wrong with the header");
+                        while(true){
+                            System.out.print("What is your email address?: ");
+                            emailInput = scanner.nextLine();
+                            if(emailInput != null && pattern.matcher(emailInput).matches()){
+                                email = emailInput;
+                                break;
+                            } else {
+                                System.out.println("Your email: " + emailInput + " , is not correct. Try again");
+                            }
+                        }
+        
+        
+                    System.out.print("What is your professional background?: ");
+                    bgInput = scanner.nextLine();
+        
+                    System.out.print("Tell us how we can help you today: ");
+                    needsInput = scanner.nextLine();
+        
+                    // add validation of date input
+                    while(true){
+                        System.out.print("What date do you want to pick?: ");
+                        futureDate = scanner.nextLine();
+                        parsedFutureDate = LocalDate.parse(futureDate);
+                        if(todayDate.isBefore(parsedFutureDate)){
+                            break;
+                        } else {
+                            System.out.println("You chose a date that has been passed. Try a future date.");
+                        }
                     }
-                } else {
-                    try {
-                        FileWriter writer = new FileWriter(file, true);
-                    writer.append(String.join(",", newSchedule) + "\n");
-                    writer.close();
-                    } catch (Exception e) {
-                        System.out.println("Something went wrong with the content");
+        
+                    // add validation of time input
+                    while (true) {   
+                        System.out.print("What time do you want to pick between 8am and 5pm?: ");
+                        timeInput = scanner.nextLine();
+                        time = LocalTime.parse(timeInput);
+                        String openingHrs = "08:00:00";
+                        String closingHrs = "17:00:00";
+                        LocalTime startTime = LocalTime.parse(openingHrs);
+                        LocalTime endTime = LocalTime.parse(closingHrs);
+            
+                        if(time.isAfter(startTime) && time.isBefore(endTime)){
+                            break;
+                        } else {
+                            System.out.println("You selected a time outside our opening hours. Try again");
+                        }
                     }
-                }
+        
+        
+                    formattedDate = parsedFutureDate.toString();
+                    formattedTime = time.toString();
+        
+                    for(int i = 0; i < newSchedules.size(); i++){
+                        if(i % 7 == 0){
+                            ID++;
+                        }
+                    }
                 
-
-        System.out.println("You have been signed up!");
-        newSchedule.clear();
-
-
-        }while(fullName.isEmpty() || email.isEmpty() || bgInput.isEmpty() || needsInput.isEmpty() || formattedDate.isEmpty() || formattedTime.isEmpty());
-
-
+                    newSchedule.addFirst(Integer.toString(ID));
+                    newSchedule.add(fullName);
+                    newSchedule.add(email);
+                    newSchedule.add(bgInput);
+                    newSchedule.add(needsInput);
+                    newSchedule.add(formattedDate);
+                    newSchedule.add(formattedTime);
+        
+                    for(String s: newSchedule){
+                    System.out.println(s);
+                }
+        
+                     newSchedules.addAll(newSchedule);
+        
+        
+                    fullName = "";
+                    email = "";
+                    bgInput = "";
+                    needsInput = "";
+                    formattedDate = "";
+                    formattedTime = "";
+        
+        
+                    String headers = "ID,Full Name,Email,Background,Need,ScheduledDate,ScheduledTime";
+        
+                        if(file.length() == 0){
+                            try(FileWriter writer = new FileWriter(file)) {
+                            writer.append(headers + "\n");
+                            writer.append(String.join(",", newSchedule) + "\n");
+                              } catch (Exception e) {
+                                System.out.println("Something went wrong with the header");
+                            }
+                        } else {
+                            try {
+                                FileWriter writer = new FileWriter(file, true);
+                            writer.append(String.join(",", newSchedule) + "\n");
+                            writer.close();
+                            } catch (Exception e) {
+                                System.out.println("Something went wrong with the content");
+                            }
+                        }
+                        
+        
+                System.out.println("You have been signed up!");
+                newSchedule.clear();
+        
+        
+                }while(fullName.isEmpty() || email.isEmpty() || bgInput.isEmpty() || needsInput.isEmpty() || formattedDate.isEmpty() || formattedTime.isEmpty());
+            }
+            if(userInput.equals("close")){
+                System.out.println("Goodbye");
+                break;
+            }
+        }
         scanner.close();
+
     }
 }
