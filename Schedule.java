fee97be11641a55f769496bcf48f0c4d7c08e28b
Emmanuel Omole <emmanueliyare7@gmail.com>
2025-11-09 12:18:09 -0500

    refactoring the update schedule task

--- Schedule.java
+++ Schedule.java
@@ -4,8 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
